fad65fdfcb020a116cf09e43c51439acf4defc88
Amyl11 <vumanhcvp@gmail.com>
2024-10-03 01:33:25 +0700

    mission complete

--- day4/dssv.h
+++ day4/dssv.h
@@ -15,20 +15,22 @@ struct vector {
     vec_elem_t *elems;
 };
 
+#define VFOR(v, cur) for (vec_elem_t *cur = (v)->elems; cur < (v)->elems + (v)->sz; cur++)
+
 struct vector *vcreate(int n);
 void vreserve(struct vector *v, int new_cap);
 void vappend(struct vector *v, vec_elem_t value);
-void vresize(struct vector *v, int newsize);
+void vresize(struct vector *v, int new_size);
 void vfree(struct vector *v);
 
-void vscan_elem(vec_elem_t *e);
 void vprint_elem(vec_elem_t *e);
 
-void vparse_marked();
+struct vector *vparse_marked();
 
-void vswap(vec_elem_t *e, vec_elem_t *t);
-void vsort(struct vector *v);
-vec_elem_t *vsearch(struct vector *v, char key[]);
+void vsort(struct vector *v, int (*order)(const void *, const void *));
+int hpdecrease(const void *a, const void *b);
+
+void vsearch(struct vector *v, char key[]);
 
 void solve(void);
 
--- day4/function.c
+++ day4/function.c
@@ -3,40 +3,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct vector *vcreate(int n){
+
+struct vector *vcreate(int n) {
     struct vector *v = malloc(sizeof(struct vector));
     v->sz = n;
-    v->cap = n>8 ? n:8;
-    v->elems = calloc(n, sizeof(vec_elem_t));
-
+    v->cap = v->sz > 8 ? v->sz : 8;
+    v->elems = calloc(v->cap, sizeof(vec_elem_t));
     return v;
 }
 
-void vreserve(struct vector *v, int new_cap){
-    if(new_cap < v->cap) {
-        printf("Something went wrong\n");
+void vreserve(struct vector *v, int new_cap) {
+    if(new_cap <= v->sz) {
         return;
     }
     v->elems = realloc(v->elems, new_cap * sizeof(vec_elem_t));
+    if(v->cap < new_cap) {
+        memset(v->elems + v->cap, 0, sizeof(vec_elem_t) * (new_cap - v->cap));
+    }
     v->cap = new_cap;
 }
 
 void vappend(struct vector *v, vec_elem_t value) {
     if(v->cap == 0) {
         vreserve(v, 16);
-    }
-    else if(v->sz == v->cap) {
+    } else if(v->cap == v->sz) {
         vreserve(v, 2 * v->cap);
     }
     v->elems[v->sz] = value;
     v->sz++;
 }
 
-void resize(struct vector *v, int newsize) {
-    if(newsize > v->cap) {
-        vreserve(v, newsize);
+void vresize(struct vector *v, int new_size) {
+    if(new_size > v->cap) {
+        vreserve(v, new_size);
     }
-    v->sz = newsize;
+    v->sz = new_size;
 }
 
 void vfree(struct vector *v) {
@@ -47,32 +48,29 @@ void vfree(struct vector *v) {
     free(v);
 }
 
-void vscan_elem(vec_elem_t *e) {
-    scanf("%d,%[^,],%[^,],%f", &e->stt, e->mssv, e->hoten, &e->hp);
-}
-
 void vprint_elem(vec_elem_t *e) {
-    printf("%d,%s,%s,%f", e->stt, e->mssv, e->hoten, e->hp);
+    printf("%d,%s,%s,%.1f\n", e->stt, e->mssv, e->hoten, e->hp);
 }
 
-struct vector *vparse_marked(){
+struct vector *vparse_marked() {
     struct vector *v = vcreate(0);
-    while (1) {
-        vec_elem_t i;
-        int z = vscan_elem(&i);
-        if(z < 4) {
+    char input[150] = {0};
+    vec_elem_t e;
+    while(1) {
+        scanf("%[^\n]%*c", input);
+        if(strcmp(input, "*****") == 0) {
             break;
         }
-        vappend(v, i);
+        sscanf(input, "%d,%[^,],%[^,],%f", &e.stt, e.mssv, e.hoten, &e.hp);
+        vappend(v, e);
     }
     return v;
 }
 
-
-void vsort(struct vector *v) {
-    for(vec_elem_t *i = v->elems; i < v->elems + v->sz; i++) {
-        for(vec_elem_t *j = v->elems; j < (vec_elem_t *)(v->elems + v->sz - i - 1); j++) {
-            if((j->hp < (j+1)->hp) || (j->hp == (j+1)->hp && j->mssv == (j+1)->mssv)) {
+void vsort(struct vector *v, int (*order)(const void *, const void *)) {
+    for(int i = 0; i < v->sz - 1; i++) {
+        for(int j = i + 1; j < v->sz; j++) {
+            if(!order(v->elems + i, v->elems + j)) {
                 vec_elem_t tmp = v->elems[i];
                 v->elems[i] = v->elems[j];
                 v->elems[j] = tmp;
@@ -81,33 +79,55 @@ void vsort(struct vector *v) {
     }
 }
 
-vec_elem_t *vsearch(struct vector *v, char key[]) {
-    for(vec_elem_t *i = v->elems; i<v->elems+v->sz; i++) {
-        if(!strcmp(i->mssv, key)) {
-            return i;
+int hpdecrease(const void *a, const void *b) {
+    const vec_elem_t *e1 = a, *e2 = b;
+    if(e1->hp < e2->hp) {
+        return 0;
+    } else if(e1->hp > e2->hp) {
+        return 1;
+    }
+    if(strcmp(e1->mssv, e2->mssv) < 0) {
+        return 1;
+    }
+    return 0;
+}
+
+void vsearch(struct vector *v, char key[]) {
+    VFOR(v, cur) {
+        if(strcmp(cur->mssv, key) == 0) {
+            vprint_elem(cur);
+            return;
         }
     }
 }
 
-
-void solve(void) {
-    char end[] = "*****";
+void solve() {
     struct vector *v = vparse_marked();
-    // printf("%d", n);
-    vsort(v);
-    for(vec_elem_t *i = v->elems; i<v->elems+v->sz; i++) {
-        vprint_elem(i);
+    vsort(v, hpdecrease);
+    printf("%d\n", v->sz);
+    VFOR(v, cur) {
+        vprint_elem(cur);
     }
 
-
-    // while (1) {
-    //     char key[10];
-    //     scanf("%s", &key);
-    //     if(!strcmp(key, end)) {
-    //         break;
-    //     }
-    //     vprint_elem(vsearch(v, key));
-    // }
-
-
+    char **data = (char **)malloc(v->sz * sizeof(char *));
+    int count = 0;
+    while(1) {
+        char tmp[150];
+        scanf("%[^\n]%*c", tmp);
+        if(strcmp(tmp, "*****") == 0) {
+            break;
+        }
+        data[count] = (char *)malloc((strlen(tmp)+1) * sizeof(char));
+        strcpy(data[count], tmp);
+        count++;
+    }
+    printf("%d\n", count);
+    for(int i=0; i<=count; i++) {
+        vsearch(v, data[i]);
+    }
+    for(int i=0; i<v->sz; i++) {
+        free(data[i]);
+    }
+    free(data);
+    vfree(v);
 }
