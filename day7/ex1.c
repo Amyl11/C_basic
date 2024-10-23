f3e836417579f5d7e8cb48d87aaac2d309b3d2e4
Amyl11 <vumanhcvp@gmail.com>
2024-10-23 10:54:24 +0700

    ex1 complete

--- day7/ex1.c
+++ day7/ex1.c
@@ -1,99 +1,137 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-struct date {
-  int y;
-  int m;
-  int d;
+struct sv {
+    char date[20];
+    char uid[20];
 };
 
-struct vec_elem_t {
-  struct date day;
-  int h;
-  int m;
-  int s;
-  char uid[15];
-  char qid[15];
-};
-
-typedef struct vec_elem_t vec_elem_t;
+typedef struct sv vec_elem_t;
 
 struct vector {
-  int sz;
-  int cap;
-  vec_elem_t *elems;
+    int sz;
+    int cap;
+    vec_elem_t *elems;
 };
 
-#define VFOR(v, cur) for (vec_elem_t *cur = (v)->elems; cur < (v)->elems + (v)->sz; cur++)
+#define VFOR(v,cur) for (vec_elem_t *cur = (v)->elems; cur < (v)->elems + (v)->sz; cur++)
+
 
 struct vector *vcreate(int n) {
-  struct vector *v = malloc(sizeof(struct vector));
-  v->sz = n;
-  v->cap = v->sz > 0? v->sz: 8;
-  v->elems = calloc(v->cap, sizeof(vec_elem_t));
-  return v;
+    struct vector *v = malloc(sizeof(struct vector));
+    v->sz = n;
+    v->cap = n > 8 ? n : 8;
+    v->elems = malloc(v->cap * sizeof(vec_elem_t));
+    return v;
 }
 
-void vreserve(struct vector *v, int newcap) {
-  if (newcap <= v->sz) {
-    return;
-  }
-  v->elems = realloc(v->elems, newcap * sizeof(vec_elem_t));
-  if (v->cap < newcap) {
-    memset(v->elems + v->cap, 0, sizeof(vec_elem_t) * (newcap - v->cap));
-  }
-  v->cap = newcap;
+void vreserve(struct vector *v, int ncap) {
+    if(ncap <= v->sz) {
+        return;
+    }
+    v->elems = realloc(v->elems, ncap * sizeof(vec_elem_t));
+    v->cap = ncap;
 }
 
-void vappend(struct vector *v, vec_elem_t value) {
-  if (v->cap == 0) {
-    vreserve(v, 16);
-  } else if (v->sz == v->cap) {
-    vreserve(v, 2 * v->cap);
-  }
-  v->elems[v->sz] = value;
-  ++v->sz;
-}
-
-void vresize(struct vector *v, int newsize) {
-  if (newsize > v->cap) {
-    vreserve(v, newsize);
-  }
-  v->sz = newsize;
+void vappend(struct vector *v, vec_elem_t e) {
+    if(v->cap == 0) {
+        vreserve(v, 16);
+    } else if(v->cap == v->sz) {
+        vreserve(v, 2 * v->cap);
+    }
+    v->elems[v->sz] = e;
+    v->sz++;
 }
 
 void vfree(struct vector *v) {
-  if (!v) {
-    return;
-  }
-  free(v->elems);
-  free(v);
-}
-
-struct vector *vscan() {
-  struct vector *v = vcreate(0);
-  char input[100];
-  while(1) {
-    scanf("%s\n", input);
-    if(strcmp(input, "*")){
-      return v;
+    if(!v) {
+        return;
     }
-    sscanf("%d-%d-%d %d:%d:%d %[^ ] %[^\n]", input, &v->elems->day.y, &v->elems->day.m, &v->elems->day.d, &v->elems->h, &v->elems->m, &v->elems->s, v->elems->uid, v->elems->qid);
-  }
+    free(v->elems);
+    free(v);
 }
 
-void vprint(struct vector *v) {
-  VFOR(v, cur) {
-    printf("%d-%d-%d %d:%d:%d %s %s\n", v->elems->day.y, v->elems->day.m, v->elems->day.d, v->elems->h, v->elems->m, v->elems->s, v->elems->uid, v->elems->qid);
-  }
-  return;
+void print_vec(struct vector *v) {
+    VFOR(v, cur) {
+        printf("%s %s\n", cur->date, cur->uid);
+    }
+}
+
+#define VSWAP(e1, e2) {\
+    vec_elem_t __tmp = e1;\
+    e1 = e2;\
+    e2 = __tmp;\
+}
+
+void heapify(struct vector *v,int root, int (*order)(char *a, char *b)) {
+    if(root > v->sz/2 -1) {
+        return;
+    }
+    int lc = 2*root +1;
+    int rc = 2*root +2;
+    heapify(v,lc,order);
+    if(!order(v->elems[root].date, v->elems[lc].date)) {
+        VSWAP(v->elems[root], v->elems[lc]);
+    }
+    if(rc==v->sz) {
+        return;
+    }
+    heapify(v,rc,order);
+    if(!order(v->elems[root].date, v->elems[rc].date)) {
+        VSWAP(v->elems[root], v->elems[rc]);
+    }
+}
+
+int max(char *a, char *b) {
+    int k = strcmp(a, b);
+    if(k>0) {
+        return 1;
+    }
+    return 0;
+}
+
+void heapsort(struct vector *v) {
+    int tmp = v->sz;
+    while(v->sz > 1) {
+        heapify(v,0,max);
+        VSWAP(v->elems[0], v->elems[v->sz-1]);
+        v->sz--;
+    }
+    v->sz = tmp;
+}
+
+void solve(struct vector *v) {
+    char tmp[20];
+    int cnt = 0;
+    strcpy(tmp, v->elems[0].date);
+    VFOR(v, cur) {
+        if(strcmp(tmp, cur->date) == 0) {
+            cnt++;
+        } else {
+            printf("%s %d\n", tmp, cnt);
+            strcpy(tmp, cur->date);
+            cnt=1;
+        }
+    }
+    printf("%s %d\n", tmp, cnt);
+}
+
+void main() {
+    struct vector *v = vcreate(0);
+    char input[100];
+    vec_elem_t elem;
+    while(scanf("%[^\n]%*c", input)) {
+        if(!strcmp(input, "*")) {
+            break;
+        }
+        sscanf(input, "%s%*s%s", elem.date, elem.uid);
+        vappend(v, elem);
+    }
+    heapsort(v);
+    solve(v);
+    vfree(v);
 }
 
 
-int main() {
-  struct vector *v = vscan(0);
-  vprint(v);
-  vfree(v);
-}
